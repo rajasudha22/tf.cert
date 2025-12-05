972bc7679b811816aa4efb9aea989a396e25e128
Rajasekhar Pandeti <raja.sudha225@gmail.com>
2025-12-05 18:22:11 +0530

    updated tf code and checkov policy

--- s3.tf
+++ s3.tf
@@ -3,10 +3,10 @@ resource "aws_s3_bucket" "app_bucket" {
   bucket = "my-app-bucket-unique-name-123"
 
   tags = {
-    Name        = "Application Bucket"
-    Environment = "production"
+    project     = "Datalake-v2"
+    env         = "production"
     Owner       = "DevOps Team"
-    # Project     = "WebApp"
+    cost-center = "12345"
   }
 }
 
